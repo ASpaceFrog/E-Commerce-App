83690cbb963234789a5a60418ec871420899e503
ASpaceFrog <filiplukic000@gmail.com>
2021-03-10 17:13:13 -0500

    -added storetest package -added StoreManagerTest.java

---
+++ src/storetest/StoreManagerTest.java
@@ -0,0 +1,50 @@
+package storetest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreManagerTest {
+
+    @Test
+    @DisplayName("Check if getMyInventory returns the correct inventory")
+    void testGetMyInventory() {
+
+    }
+
+    @Test
+    @DisplayName("")
+    void testGetUserCarts() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testCheckStock() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testNewShoppingCart() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testAddToCart() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testRemoveFromCart() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testEmptyCart() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testPrintInventory() {
+    }
+}
