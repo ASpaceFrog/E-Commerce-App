b014ff4a20ae322a34048d39a15d16fbab8b2c5e
ASpaceFrog <filiplukic000@gmail.com>
2021-03-21 12:44:34 -0400

    added javadoc for StoreManagerTest.java

--- src/storetest/StoreManagerTest.java
+++ src/storetest/StoreManagerTest.java
@@ -23,11 +23,17 @@ class StoreManagerTest{
 
     private static StoreManager sm;
 
+    /**
+     * Reset StoreManager sm to its default state before each test
+     */
     @BeforeEach
     public void init(){
         sm = new StoreManager();
     }
 
+    /**
+     * Test getMyInventory method in the StoreManager Class
+     */
     @Test
     public void testGetMyInventory() {
         new StoreManager();
@@ -52,12 +58,19 @@ class StoreManagerTest{
         assertEquals(pInfo, sm.getMyInventory().getProductInfo(), "Field init in StoreManager is not working");
     }
 
+
+    /**
+     * Test getUserCarts method in the StoreManager Class
+     */
     @Test
     public void testGetUserCarts() {
         ArrayList<ShoppingCart> uc = new ArrayList<>();
         assertEquals(uc, sm.getUserCarts(), "Getter not working properly");
     }
 
+    /**
+     * Test method to check the stock of a product in the StoreManager Class
+     */
     @Test
     public void testCheckStock() {
         Product p1 = new Product("apple", 1, 2.00); // Same arguments as the Product in StoreManager
@@ -69,12 +82,18 @@ class StoreManagerTest{
         assertEquals(-1, sm.checkStock(p3), "Field init in StoreManager is not working");
     }
 
+    /**
+     * Test creating a new shopping cart in the StoreManager Class
+     */
     @Test
     public void testNewShoppingCart() {
         assertEquals(0, sm.newShoppingCart(), "newShoppingCart not working"); // First ShoppingCart will have id 0
         assertEquals(1, sm.newShoppingCart(), "newShoppingCart not working"); // Next ShoppingCart will have id 1
     }
 
+    /**
+     * Test adding a specified amount of a product to a cart in the StoreManager Class
+     */
     @Test
     public void testAddToCart() {
         sm.newShoppingCart(); // Create new cart, id will be 0
@@ -83,6 +102,9 @@ class StoreManagerTest{
         assertFalse(sm.addToCart(0, 5, 5), "Field init in StoreManager is not working or addToCart is not working");
     }
 
+    /**
+     * Test removing a specified amount of a product to a cart in the StoreManager Class
+     */
     @Test
     public void testRemoveFromCart() {
         sm.newShoppingCart();  // Create new cart, id will be 0
@@ -96,6 +118,9 @@ class StoreManagerTest{
 
     }
 
+    /**
+     * Test emptying a cart in the StoreManager Class
+     */
     @Test
     public void testEmptyCart() {
         sm.newShoppingCart();  // Create new cart, id will be 0
