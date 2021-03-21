bc91408182a2e04b43fc3dc3955f03e09cc49e80
Stefan Lukic <stefanlukic000@gmail.com>
2021-03-21 12:20:22 -0400

    javadoc

--- src/storetest/InventoryTest.java
+++ src/storetest/InventoryTest.java
@@ -3,10 +3,12 @@ package storetest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import store.Inventory;
 import store.Product;
+
 import java.util.HashMap;
 
 /**
@@ -21,6 +23,10 @@ class InventoryTest {
     private static int[] quantities;
     private static Inventory inv;
 
+
+    /**
+     * Create 2 products that we will use for testing, an array containing the products, and an array containing the quantities
+     */
     @BeforeAll
     public static void init() {
         p1 = new Product("book", 0, 10);
@@ -29,11 +35,17 @@ class InventoryTest {
         quantities = new int[]{15, 100};
     }
 
+    /**
+     * Create an Inventory with products p1 and p2 from above before each test
+     */
     @BeforeEach
-    public void initInv(){
+    public void initInv() {
         inv = new Inventory(products, quantities);
     }
 
+    /**
+     * Test getProductQuantity by comparing it with an equivalent hashmap
+     */
     @Test
     public void TestGetProductQuantity() {
         HashMap<Integer, Integer> h1 = new HashMap<>();
@@ -44,6 +56,9 @@ class InventoryTest {
         assertEquals(h1, inv.getProductQuantity(), "inv constructor or inv.getProductQuantity() is not working!");
     }
 
+    /**
+     * Test getProductInfo by comparing it with an equivalent hashmap
+     */
     @Test
     public void TestGetProductInfo() {
         HashMap<Integer, Product> h2 = new HashMap<>();
@@ -54,6 +69,9 @@ class InventoryTest {
         assertEquals(h2, inv.getProductInfo(), "inv constructor or inv.getProductInfo() is not working!");
     }
 
+    /**
+     * Test getStock to see if it returns the correct value
+     */
     @Test
     public void TestGetStock() {
         assertEquals(15, inv.getStock(0), "inv.getStock is not returning the correct value!");
@@ -61,6 +79,9 @@ class InventoryTest {
         assertEquals(-1, inv.getStock(2), "inv.getStock is not returning the correct value!");
     }
 
+    /**
+     * Test addStock to see if it properly adds the right stock to an inventory
+     */
     @Test
     public void TestAddStock() {
         Inventory inv2 = new Inventory();
@@ -71,6 +92,9 @@ class InventoryTest {
         assertEquals(100, inv2.getStock(3), "inv.addStock() is not adding the correct amount of stock!!");
     }
 
+    /**
+     * Test removeStock to see if it properly removes the correct amount of stock and deletes the product if requested
+     */
     @Test
     public void TestRemoveStock() {
         inv.removeStock(0, 3, false);
@@ -86,6 +110,9 @@ class InventoryTest {
         assertEquals(0, inv.getStock(3), "inv.removeStock() is removing products when it should not be doing so!");
     }
 
+    /**
+     * Check to see if remove product removes products from an inventory
+     */
     @Test
     public void TestRemoveProduct() {
         inv.removeProduct(-42); //this should not crash the program
@@ -94,6 +121,9 @@ class InventoryTest {
         assertEquals(-1, inv.getStock(0), "inv.removeProduct() is not removing products or is returning the wrong value!");
     }
 
+    /**
+     * Check if getInfo is returning the correct information
+     */
     @Test
     public void TestGetInfo() {
         assertNull(inv.getInfo(-42), "inv.getInfo() is not working!");
